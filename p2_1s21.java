64e0893ddf2bda6891727196e9cddee2cfea38a7
AndreaChaconL <144179138+AndreaChaconL@users.noreply.github.com>
2023-09-05 16:07:36 -0600

    practica 1 y practica 2 del dia 5 de septiembre

---
+++ p2_1s21.java
@@ -0,0 +1,9 @@
+class p2_1s21{
+	public static void main(String[] args) {
+		System.out.println("Andrea Isabel Chacon Loredo");
+		System.out.println("18 años");
+		System.out.println("andychalo55@gmail.com");
+		System.out.println("ver series");
+		System.out.println("vino");
+	}
+}
