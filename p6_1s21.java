1530161ede9dfea35afd5172e19f0acb77932a8d
Andrea Isabel Chacon Loredo <144179138+AndreaChaconL@users.noreply.github.com>
2023-09-12 15:57:53 -0600

    Practica 5 y 6 del 12 de septiembre

---
+++ p6_1s21.java
@@ -0,0 +1,11 @@
+class p6_1s21{
+	public static void main(String[] args){
+	String nombre="Andrea";
+	int edad=18;
+
+	System.out.println("El valor para edad es "+ nombre);
+	System.out.println("La edad es "+ edad);
+	System.out.println("Puede proseguir programando");
+
+	}
+}
